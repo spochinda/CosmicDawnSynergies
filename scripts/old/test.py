2ebbd7938354d783182e48d267657df20536c543
Simon Pochinda <sp2053@cam.ac.uk>
2023-10-17 11:22:50 +0100

    correcting branch diffs

--- scripts/old/test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-loss = np.loadtxt("data/globalemu/emulator4/results/loss_history.txt")
-x=range(loss.size)
-
-fig,ax = plt.subplots(nrows=1,ncols=1)
-ax.plot(x,loss,marker="o",ls="solid")
-plt.savefig("test.png")
